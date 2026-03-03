7c6ca05bca830da8f446d090059e209c9ccb70bd
vladoje <vladorakic07@gmail.com>
2026-03-03 21:41:20 +0100

    Sign up with Google

--- frontend/app/components/Register.tsx
+++ frontend/app/components/Register.tsx
@@ -97,7 +97,9 @@ export default function Register() {
     toast.success("Uspješno ste se registrovali!");
     navigate("/");
   }
-
+  function handleGoogleLogin() {
+    window.location.href = "https://projekat-testovi.onrender.com/auth/google";
+  }
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center px-6 py-12">
       <div className="sm:mx-auto sm:w-full sm:max-w-md text-center">
@@ -194,6 +196,13 @@ export default function Register() {
             className="w-full flex justify-center py-4 px-4 border border-transparent rounded-2xl shadow-md text-white font-black bg-indigo-600 hover:bg-indigo-700 transition-all active:scale-95"
           >
             Napravite nalog
+          </button>
+          <button
+            onClick={handleGoogleLogin}
+            className="w-full py-4 mt-4 rounded-2xl border border-gray-300 bg-white hover:bg-gray-100 text-gray-900 font-bold shadow-md flex items-center justify-center space-x-2 transition-transform active:scale-95"
+          >
+            <img src="/google-logo.svg" alt="Google logo" className="w-5 h-5" />
+            <span>Sign up with Google</span>
           </button>
         </div>
 
